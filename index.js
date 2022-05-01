c3030ffe1cd1d14bdb64032a3271899decd7c3de
Pischedda <dylan.pischedda@hotmail.com>
2022-05-01 18:30:04 +0200

    [Feat] Récupération des valeurs des dès selon le joueurs en jeu

--- index.js
+++ index.js
@@ -19,8 +19,26 @@ $(document).ready(function() {
 
 function closeModalAndPlay() {
     $('#StartModal').modal('hide');
-    startTimer(5);
+    startTimer(590);
     leftBack.hidden = false;
     leftCache.hidden = false;
     startBackground.hidden = true;
+}
+
+function actualPlayerFunction() {
+    var Player;
+    leftBack.hidden ? Player = "PlayerOne" : Player = "PlayerTwo"
+    return Player;
+}
+
+function GetValueDice(Player) {
+    var PlayerDice = document.getElementsByClassName("" + Player);
+    PlayerDice = PlayerDice[0].firstElementChild.children;
+    var allNumber = [];
+
+    for (let i = 0; i < PlayerDice.length; i++) {
+        var number = PlayerDice[i].firstElementChild.firstElementChild.children[1].classList.value;
+        allNumber.push(number)
+    }
+    console.log(Player + " : " + allNumber)
 }
